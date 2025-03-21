241cc8b8215385ea4a82717fc13e1648d57c62b7
ZormaX <81564791+ZormamaX@users.noreply.github.com>
2025-03-21 22:44:42 +0200

    Simplified code
    Simplified code and added comments

--- colorpicker.sh
+++ colorpicker.sh
@@ -1,2 +1,9 @@
-OUTPUT="$(gpick -so)"
-echo -n "${OUTPUT}" | xclip -selection clipboard
+# echo -n is used to remove the newline that gpick likes to output
+
+# This one copies the color to XA_CLIPBOARD
+echo -n "$(gpick -so)" | xclip -selection clipboard
+
+# This one copies to XA_PRIMARY (middle-click) instead
+#echo -n "$(gpick -so)" | xclip
+
+# Or you could uncomment both to have both options
